e3afede6a8465c397c85f86b9abd0452d4dbc938
AmitMish <AmitMish@LP-5CD7490BQG.HCLT.CORP.HCL.IN>
2019-08-23 16:13:52 +0530

    update

--- src/test/java/com/ulta/cart/controller/CartControllerTest.java
+++ src/test/java/com/ulta/cart/controller/CartControllerTest.java
@@ -3,18 +3,19 @@
  * http://www.ulta.com
  * AmitMish@ulta.com
  * All rights reserved
- */ 
-  
+ */
+
 package com.ulta.cart.controller;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -35,47 +36,54 @@ import io.sphere.sdk.queries.PagedQueryResult;
 @SpringBootTest
 public class CartControllerTest {
 
-	CartController cartController=new CartController();
+	CartController cartController = new CartController();
 	@Mock
 	CartService cartService;
-	
+
 	@Before
 	public void init() {
 		MockitoAnnotations.initMocks(this);
 		cartController.setCartService(cartService);
 	}
-	
+
 	@Test
-	public void addToCartTest() {
-		CreateCartRequest requestDto=new CreateCartRequest();
-		CompletableFuture<Cart> fetchedCart = new CompletableFuture<Cart>();
+	public void addToCartTest() throws CartException, InterruptedException, ExecutionException {
+		CreateCartRequest requestDto = new CreateCartRequest();
+		Cart fetchedCart = Mockito.mock(Cart.class);
 		when(cartService.addToCart(requestDto)).thenReturn(fetchedCart);
-		ResponseEntity<CompletableFuture<Cart>> result=cartController.addItemToCart(requestDto);
-		assertEquals(HttpStatus.OK, result.getStatusCode());		
+		ResponseEntity<Cart> result = cartController.addItemToCart(requestDto);
+		assertEquals(HttpStatus.OK, result.getStatusCode());
 	}
-	
+
 	@Test(expected = CartException.class)
-	public void addToCartExceptionTest() {
-		CreateCartRequest requestDto=new CreateCartRequest();
+	public void addToCartExceptionTest() throws CartException, InterruptedException, ExecutionException {
+		CreateCartRequest requestDto = new CreateCartRequest();
 		when(cartService.addToCart(requestDto)).thenThrow(new CartException("failure"));
 		cartController.addItemToCart(requestDto);
 	}
-	
+
 	@Test
-	public void getAllCartsTest() {
-		CompletableFuture<PagedQueryResult<Cart>> carts=new CompletableFuture<PagedQueryResult<Cart>>();
+	public void getAllCartsTest() throws CartException, InterruptedException, ExecutionException {
+		PagedQueryResult<Cart> carts = (PagedQueryResult<Cart>) Mockito.mock(PagedQueryResult.class);
 		when(cartService.getAllCarts()).thenReturn(carts);
-		ResponseEntity<CompletableFuture<PagedQueryResult<Cart>>> result=cartController.getAllCarts();
-		assertEquals(HttpStatus.OK, result.getStatusCode());		
+		ResponseEntity<PagedQueryResult<Cart>> result = cartController.getAllCarts();
+		assertEquals(HttpStatus.OK, result.getStatusCode());
 	}
-	
+
+	@Test(expected = CartException.class)
+	public void getAllCartsExceptionTest() throws CartException, InterruptedException, ExecutionException {
+		PagedQueryResult<Cart> carts = (PagedQueryResult<Cart>) Mockito.mock(PagedQueryResult.class);
+		when(cartService.getAllCarts()).thenThrow(new CartException("failure"));
+		cartController.getAllCarts();
+	}
+
 	@Test
 	public void removeItemFromCartTest() {
-		CompletableFuture<Cart> carts=new CompletableFuture<Cart>();
-		RemoveLineItemRequest removeLineItemRequest=new RemoveLineItemRequest();
+		Cart carts = Mockito.mock(Cart.class);
+		RemoveLineItemRequest removeLineItemRequest = new RemoveLineItemRequest();
 		when(cartService.removeLineItem(removeLineItemRequest)).thenReturn(carts);
-		ResponseEntity<CompletableFuture<Cart>> result=cartController.removeItemFromCart(removeLineItemRequest);
-		assertEquals(HttpStatus.OK, result.getStatusCode());		
+		ResponseEntity<Cart> result = cartController.removeItemFromCart(removeLineItemRequest);
+		assertEquals(HttpStatus.OK, result.getStatusCode());
 	}
 
 }
